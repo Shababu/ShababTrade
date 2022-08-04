1474fdf9cb4b3e0342e4b4bc1f9e8b820ede7b04
Александр <skinnyrus@mail.ru>
2022-08-04 22:46:31 +0300

    Added chart

--- ViewModels/SpotViewModel.cs
+++ ViewModels/SpotViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using BinanceApiLibrary;
+using FancyCandles;
 using ShababTrade.Data.Models;
 using ShababTrade.Models;
 using ShababTrade.ViewModels.Base;
@@ -34,6 +35,18 @@ namespace ShababTrade.ViewModels
 
         #endregion
 
+        #region Chart Properties
+
+        private ICandlesSource _spotCandles = new CandlesSource(TimeFrame.M5);
+
+        public ICandlesSource SpotCandles
+        {
+            get => _spotCandles;
+            set { Set(ref _spotCandles, value); }
+        }
+
+        #endregion
+
         #endregion
 
         #region Async Methods
@@ -53,6 +66,35 @@ namespace ShababTrade.ViewModels
             }      
         }
 
+        public async void GetChartData()
+        {
+            TimeFrame timeFrame = FancyCandles.TimeFrame.M5;
+            CandlesSource candles = new CandlesSource(timeFrame);
+
+            while (true)
+            {
+                List<TradingCommonTypes.ICandle> exchangeCandles = new List<TradingCommonTypes.ICandle>();
+
+                try
+                {
+                    exchangeCandles = AppUser.MarketInfo.GetCandles(Symbol24Stats.Symbol, "5m", 500);
+                }
+
+                catch { continue; }
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    candles.Clear();
+                    foreach (var candle in exchangeCandles)
+                    {
+                        candles.Add(new Candle(candle.OpenTime, candle.Open, candle.High, candle.Low, candle.Close, candle.Volume));
+                    }
+                });
+
+                SpotCandles = candles;
+            }
+        }
+
         #endregion
 
         public SpotViewModel(List<UserLoginInfo> appUsers, string selectedExchange)
@@ -65,8 +107,11 @@ namespace ShababTrade.ViewModels
                 AvailableExchanges.Add(user.Exchange);
             }
 
+            Symbol24Stats.Symbol = "XRPUSDT";
             var statsGetter = new Thread(Get24Stats);
+            var candlesGetter = new Thread(GetChartData);
             statsGetter.Start();
+            candlesGetter.Start();
         }
     }
 }
